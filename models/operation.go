8931773ba6741dd61d210a59c59c01e5c714dc8d
Grigorii Eidelkind <gr.eydelkind@gmail.com>
2025-02-17 22:07:33 +0100

    Changeb database

--- models/operation.go
+++ models/operation.go
@@ -1,10 +1,10 @@
 package models
 
 type Operation struct {
-	ID 		   int64  `json:"id" field:"id"`
-	Name       string `json:"name" field:"name"`
-	WalletID   int64  `json:"wallet_id" field:"walletid"`
-	Sum        int64  `json:"sum" field:"sum"`
-	Date       string `json:"date" field:"date"`
-	CategoryID int64  `json:"category_id" field:"categoryid"`
+	ID         int64   `json:"id" field:"id"`
+	Name       string  `json:"name" field:"name"`
+	WalletID   int64   `json:"wallet_id" field:"walletid"`
+	Sum        float64 `json:"sum" field:"sum"`
+	Date       string  `json:"date" field:"date"`
+	CategoryID int64   `json:"category_id" field:"categoryid"`
 }
