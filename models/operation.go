f6002ccf9bbb817cb982e38f617409747f74a1a5
Grigorii Eidelkind <gr.eydelkind@gmail.com>
2025-04-20 18:23:00 +0300

    Fixed a bug in creation of operations

--- models/operation.go
+++ models/operation.go
@@ -4,7 +4,7 @@ type Operation struct {
 	ID         int64   `json:"id" field:"id"`
 	Name       string  `json:"name" validate:"omitempty,max=50" field:"name"`
 	WalletID   int64   `json:"wallet_id" field:"walletid"`
-	Sum        float64 `json:"sum" field:"sum"`
+	Sum        float64 `json:"sum" validate:"nonzero" field:"sum"`
 	Date       string  `json:"date" validate:"omitempty,datetime=2006-01-02" field:"date"`
 	Place      string  `json:"place" validate:"omitempty,max=50" field:"place"`
 	CategoryID int64   `json:"category_id" field:"categoryid"`
