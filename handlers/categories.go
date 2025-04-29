cccd503d6ca4456fa7d608e9c01e8add0746ad66
Grigorii Eidelkind <gr.eydelkind@gmail.com>
2025-04-29 20:24:54 +0200

    Refactored handlers/categories.go

--- handlers/categories.go
+++ handlers/categories.go
@@ -2,115 +2,84 @@ package handlers
 
 import (
 	"finances-backend/models"
-	"finances-backend/services"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func (h *Handler) CreateCategory(c *fiber.Ctx) error {
-	userID := c.Locals("user_id").(int64)
+	userID := c.Locals("userId").(int64)
+
 	category := models.Category{}
 	if err := h.parseBody(c, &category); err != nil {
-		c.JSON(ErrorResponse{Error: ErrWrongFormat.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusBadRequest)
+		return h.sendError(c, ErrWrongFormat, err)
 	}
 
 	category.OwnerID = userID
 	id, err := h.categoryService.CreateCategory(&category)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrCannotCreateCategory.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return h.sendError(c, ErrCannotCreateCategory, err)
 	}
 
-	c.JSON(fiber.Map{"id": id})
-	return c.SendStatus(fiber.StatusOK)
+	return h.send(c, fiber.StatusCreated, fiber.Map{"id": id})
 }
 
 func (h *Handler) GetCategories(c *fiber.Ctx) error {
-	userID := c.Locals("user_id").(int64)
+	userID := c.Locals("userId").(int64)
 	categories, err := h.categoryService.GetAllCategories(userID)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrCannotGetCategories.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return h.sendError(c, ErrCannotGetCategories, err)
 	}
-	return c.JSON(categories)
+
+	return h.send(c, fiber.StatusOK, categories)
 }
 
 func (h *Handler) GetCategoryByID(c *fiber.Ctx) error {
-	userID := c.Locals("user_id").(int64)
+	userID := c.Locals("userId").(int64)
 	categoryID, err := h.parseID(c)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrWrongFormat.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusBadRequest)
+		return h.sendError(c, ErrWrongFormat, err)
 	}
 
 	category, err := h.categoryService.GetCategoryByID(int64(categoryID), userID)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrCannotGetCategory.Error(), Description: err.Error()})
-		if err == services.ErrCategoryNotFound {
-			return c.SendStatus(fiber.StatusNotFound)
-		}
-		if err == services.ErrAccessDenied {
-			return c.SendStatus(fiber.StatusForbidden)
-		}
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return h.sendError(c, ErrCannotGetCategory, err)
 	}
 
-	return c.JSON(category)
+	return h.send(c, fiber.StatusOK, category)
 }
 
 func (h *Handler) UpdateCategory(c *fiber.Ctx) error {
-	userID := c.Locals("user_id").(int64)
+	userID := c.Locals("userId").(int64)
 	categoryID, err := h.parseID(c)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrWrongFormat.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusBadRequest)
+		return h.sendError(c, ErrWrongFormat, err)
 	}
 
 	category := models.Category{}
 	if err := h.parseBody(c, &category); err != nil {
-		c.JSON(ErrorResponse{Error: ErrWrongFormat.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusBadRequest)
+		return h.sendError(c, ErrWrongFormat, err)
 	}
 
 	category.ID = int64(categoryID)
 	err = h.categoryService.UpdateCategory(&category, userID)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrCannotUpdateCategory.Error(), Description: err.Error()})
-		if err == services.ErrCategoryNotFound {
-			return c.SendStatus(fiber.StatusNotFound)
-		}
-		if err == services.ErrAccessDenied {
-			return c.SendStatus(fiber.StatusForbidden)
-		}
-		// Handle other errors (e.g., database errors)
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return h.sendError(c, ErrCannotUpdateCategory, err)
 	}
 
-	c.JSON(category)
-	return c.SendStatus(fiber.StatusOK)
+	return h.send(c, fiber.StatusOK, category)
 }
 
 func (h *Handler) DeleteCategory(c *fiber.Ctx) error {
-	userID := c.Locals("user_id").(int64)
+	userID := c.Locals("userId").(int64)
 	categoryID, err := h.parseID(c)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrWrongFormat.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusBadRequest)
+		return h.sendError(c, ErrWrongFormat, err)
 	}
 
 	err = h.categoryService.DeleteCategory(int64(categoryID), userID)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrCannotDeleteCategory.Error(), Description: err.Error()})
-		if err == services.ErrCategoryNotFound {
-			return c.SendStatus(fiber.StatusNotFound)
-		}
-		if err == services.ErrAccessDenied {
-			return c.SendStatus(fiber.StatusForbidden)
-		}
-		// Handle other errors (e.g., database errors)
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return h.sendError(c, ErrCannotDeleteCategory, err)
 	}
 
-	return c.SendStatus(fiber.StatusOK)
+	return h.send(c, fiber.StatusNoContent, nil)
 }
