7e61dd11b61173d912e0759fd825bb5dec7a545f
Grigorii Eidelkind <gr.eydelkind@gmail.com>
2025-04-29 21:09:41 +0200

    Refactored handlers/wallet.go

--- handlers/wallets.go
+++ handlers/wallets.go
@@ -11,80 +11,69 @@ func (h *Handler) CreateWallet(c *fiber.Ctx) error {
 
 	wallet := models.Wallet{}
 	if err := h.parseBody(c, &wallet); err != nil {
-		c.JSON(ErrorResponse{Error: ErrWrongFormat.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusBadRequest)
+		return h.sendError(c, ErrWrongFormat, err)
 	}
 
 	// Check if both fields are provided
 	if len(wallet.Name) == 0 || len(wallet.Currency) == 0 {
-		c.JSON(ErrorResponse{Error: ErrWrongFormat.Error(), Description: "Not all required fields provided"})
-		return c.SendStatus(fiber.StatusBadRequest)
+		return h.sendError(c, ErrWrongFormat, ErrNotAllRequiredFieldsProvided)
 	}
 
 	wallet.OwnerID = userID
 	id, err := h.walletService.CreateWallet(&wallet)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrCannotCreateWallet.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return h.sendError(c, ErrCannotCreateWallet, err)
 	}
 
-	return c.JSON(fiber.Map{"id": id})
+	return h.send(c, fiber.StatusCreated, fiber.Map{"id": id})
 }
 
 func (h *Handler) GetWallets(c *fiber.Ctx) error {
 	userID := c.Locals("userId").(int64)
 	wallets, err := h.walletService.GetAllWallets(userID)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrCannotGetWallets.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return h.sendError(c, ErrCannotGetWallets, err)
 	}
-	return c.JSON(wallets)
+	return h.send(c, fiber.StatusOK, wallets)
 }
 
 func (h *Handler) GetWalletByID(c *fiber.Ctx) error {
 	userID := c.Locals("userId").(int64)
 	walletID, err := h.parseID(c)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrWrongFormat.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusBadRequest)
+		return h.sendError(c, ErrWrongFormat, err)
 	}
 
 	wallet, err := h.walletService.GetWalletByID(int64(walletID), userID)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrCannotGetWallet.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusNotFound)
+		return h.sendError(c, ErrCannotGetWallet, err)
 	}
 
-	return c.JSON(wallet)
+	return h.send(c, fiber.StatusOK, wallet)
 }
 
 func (h *Handler) UpdateWallet(c *fiber.Ctx) error {
 	userID := c.Locals("userId").(int64)
 	walletID, err := h.parseID(c)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrWrongFormat.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusBadRequest)
+		return h.sendError(c, ErrWrongFormat, err)
 	}
 
 	wallet := models.Wallet{}
 	if err := h.parseBody(c, &wallet); err != nil {
-		c.JSON(ErrorResponse{Error: ErrWrongFormat.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusBadRequest)
+		return h.sendError(c, ErrWrongFormat, err)
 	}
 
 	// Check if at least one field is provided
 	if len(wallet.Name) == 0 && len(wallet.Currency) == 0 {
-		c.JSON(ErrorResponse{Error: ErrWrongFormat.Error(), Description: "Not all required fields provided"})
-		return c.SendStatus(fiber.StatusBadRequest)
+		return h.sendError(c, ErrWrongFormat, ErrNotAllRequiredFieldsProvided)
 	}
 
 	wallet.ID = int64(walletID)
 	err = h.walletService.UpdateWallet(&wallet, userID)
 	if err != nil {
-		c.JSON(ErrorResponse{Error: ErrCannotUpdateWallet.Error(), Description: err.Error()})
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return h.sendError(c, ErrCannotUpdateWallet, err)
 	}
 
-	c.JSON(wallet)
-	return c.SendStatus(fiber.StatusOK)
+	return h.send(c, fiber.StatusOK, nil)
 }
