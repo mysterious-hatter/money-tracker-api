1954326a39ba0e2b9527eb5d3bc1e4efad1d5068
Grigorii Eidelkind <gr.eydelkind@gmail.com>
2025-04-30 18:17:39 +0200

    Added currency validation

--- models/wallet.go
+++ models/wallet.go
@@ -4,6 +4,6 @@ type Wallet struct {
 	Id       int64   `json:"id" field:"id"`
 	OwnerId  int64   `json:"owner_id" field:"ownerid"`
 	Name     string  `json:"name" validate:"omitempty,max=50" field:"name"`
-	Currency string  `json:"currency" validate:"omitempty,max=50" field:"currency"`
+	Currency string  `json:"currency" validate:"omitempty,iso4217" field:"currency"`
 	Balance  float64 `json:"balance" field:"balance"`
 }
