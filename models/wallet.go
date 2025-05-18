5a3792e97df390f2b425e2764adc942864cf4d98
Grigorii Eidelkind <gr.eydelkind@gmail.com>
2025-05-18 12:08:51 +0200

    Fixed a bug in wallet creation

--- models/wallet.go
+++ models/wallet.go
@@ -5,5 +5,5 @@ type Wallet struct {
 	OwnerId  int64   `json:"ownerId" gorm:"<-:create;column:ownerid"`
 	Name     string  `json:"name" validate:"omitempty,max=50" gorm:"<-"`
 	Currency string  `json:"currency" validate:"omitempty,iso4217" gorm:"<-"`
-	Balance  float64 `json:"balance" field:"balance"`
+	Balance  float64 `json:"balance" field:"balance" gorm:"->"`
 }
